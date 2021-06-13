15ae54f06d5b6fbc45698ef20574ea89254df21f
Aadit Rahul Kamat <aadit.k12@gmail.com>
2021-06-13 18:45:57 +0800

    Remove docs config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,12 +81,6 @@ module.exports = {
     [
       '@docusaurus/preset-classic',
       {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
