a90fdbeb742ce0202500c5eb0eb620cfaba8443e
Aadit Rahul Kamat <aadit.k12@gmail.com>
2021-06-24 08:58:32 +0800

    Remove docusaurus plugin configs

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/aaditkamat/Projects/aaditkamat.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/aaditkamat/Projects/aaditkamat.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/aaditkamat/Projects/aaditkamat.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/aaditkamat/Projects/aaditkamat.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/aaditkamat/Projects/aaditkamat.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/aaditkamat/Projects/aaditkamat.github.io/src/css/custom.css"),
-];
