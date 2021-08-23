ccba46ee38a8a824978aebf1ae4a34b437d8b1f8
Aadit Rahul Kamat <aadit.k12@gmail.com>
2021-08-23 23:26:09 +0800

    Add resume

---
+++ src/pages/resume/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import styles from './styles.module.css';
+
+export default function Resume() {
+    return (
+        <Layout title="Resume">
+            <div className={clsx("container", styles.container)}>
+                <h1 className={styles.header}> RESUME </h1>
+                <iframe src="/docs/resume.pdf" width="100%" height="100%" />
+            </div>
+        </Layout>
+    );
+}
