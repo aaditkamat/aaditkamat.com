ea8926022ac861a01e56458cb539288c0c4d16c1
Aadit Rahul Kamat <aadit.k12@gmail.com>
2021-08-23 23:27:06 +0800

    Add links to different sections

--- src/pages/about-me/index.js
+++ src/pages/about-me/index.js
@@ -19,27 +19,25 @@ export default function AboutMe() {
                             <p>  
                             I'm currently working full time as a QA Engineer in the Android Front End Automation Platform team at Shopee.
                             I recently graduated from the National University of Singapore with a Bachelor of Computing
-                            in Computer Science. I love to code and contribute to open source projects when I can.
+                            in Computer Science. I love to code and <a href="https://github.com/aaditkamat">contribute to open source projects </a> when I can.
                             </p>
                             <p>
                             Apart from coding, I also like to read books, online articles and blogs as well as 
                             watch YouTube videos and podcasts to enhance my knowledge on topics that interest me
-                            and provide new perspectives. I've linked some of my favourite resources in the "Top 10's"
-                            section of the website. 
+                            and provide new perspectives. I've linked some of my favourite resources in the 
+                            <a href="/top10"> Top 10's</a> section of the website. 
                             </p>
                             <p>
-                            I'm also trying my hand at writing blog posts, so do be sure to check
+                            Recently, I've been trying my hand at writing <a href="/blog">blog posts</a>, so do be sure to check
                             out that section of the website as well.
                             </p>
                             <p>
-                            Last but not the least, I've described some of the projects I've worked on in the past in a seperate
+                            I've also described some of the <a href="/projects">projects</a> I've worked on in the past in a seperate
                             section.
                             </p>
-                            <Link
-                                className="button button--secondary button--lg"
-                                to="/about-me">
-                                Resume
-                            </Link>
+                            <p>
+                            Last but not the least, you can check out my <a href="/resume">resume</a> here.
+                            </p>
                             </div>
                         </div>
                         <div className="row" style={{padding: '3em'}}></div>
