c2003b6c710c8031278f4c9e1688d8b260c6b8e1
Aadit Kamat <aadit.k12@gmail.com>
2022-03-15 08:28:55 +0800

    Update server address for cypress test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,6 @@
 it('works', () => {
    expect(42).to.equal(21 + 21);
-   cy.visit('aaditkamat.com').then(() => {
+   cy.visit('http://aaditkamat.com').then(() => {
         expect('hello').to.equal('hello');
    });
 })
