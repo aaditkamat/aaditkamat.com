9dd1f359db88ed1815cd1ff763b00706ce4e24cd
Aadit Kamat <aadit.k12@gmail.com>
2022-03-27 04:09:02 +0000

    Remove react icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import styles from './index.module.css';
-import {
-  SiLinkedin,
-  SiQuora,
-  SiTwitter,
-  SiGithub,
-  SiMedium,
-  SiFacebook,
-  SiInstagram,
-} from 'react-icons/si';
 
 export default function Home() {
   return (
@@ -20,33 +11,6 @@ export default function Home() {
             Hello world, this is Aadit.
             <br /> <br />
             Welcome to my 🏠 on the internet.
-            <br /> <br />
-            You can also find me on:
-            <br /> <br />
-            <a href="https://linkedin.com/in/aaditkamat">
-              <SiLinkedin className={styles.icon} />
-            </a>
-            <a href="https://github.com/aaditkamat">
-              <SiGithub className={styles.icon} />
-            </a>
-            <a href="https://devpost.com/aaditkamat">
-              <img src="img/devpost.svg" alt="Devpost icon" className={styles.icon} />
-            </a>
-            <a href="https://quora.com/profile/Aadit-Kamat">
-              <SiQuora className={styles.icon} />
-            </a>
-            <a href="https://medium.com/aaditkamat">
-              <SiMedium className={styles.icon} />
-            </a>
-            <a href="https://twitter.com/aaditkamat">
-              <SiTwitter className={styles.icon} />
-            </a>
-            <a href="https://instagram.com/aadit_kamat">
-              <SiInstagram className={styles.icon} />
-            </a>
-            <a href="https://www.facebook.com/AaditKam">
-              <SiFacebook className={styles.icon} />
-            </a>
           </div>
         </div>
       </section>
