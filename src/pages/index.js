7f1f7d98306fec0d814ebf43541c79d30bc41e12
Aadit Kamat <aadit.k12@gmail.com>
2022-03-27 06:24:46 +0000

    Change reference to style sheet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import styles from './index.module.css';
+import styles from './styles.module.css';
 
 export default function Home() {
   return (
