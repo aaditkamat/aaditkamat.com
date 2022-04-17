e9eae8e2945afb1e507da420bf2c1133008411df
Aadit Kamat <aadit.k12@gmail.com>
2022-04-17 03:21:11 +0000

    Fix CodeFactor style issues

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,8 +20,8 @@ module.exports = {
       src: 'https://instant.page/5.1.0',
       async: true,
       type: 'module',
-      integrity: 'sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw'
-    }
+      integrity: 'sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw',
+    },
   ],
   themeConfig: {
     colorMode: {
