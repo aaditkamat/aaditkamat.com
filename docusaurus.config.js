a6a1a89430a56800e0f6dd8d95c8ae4b61080f45
Aadit Kamat <aadit.k12@gmail.com>
2022-04-17 03:18:55 +0000

    Fix typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     {
       src: 'https://instant.page/5.1.0',
-      aync: true,
+      async: true,
       type: 'module',
       integrity: 'sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw'
     }
