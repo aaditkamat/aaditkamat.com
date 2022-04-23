738e32857cd543aba9813dcc33d99ff78c42264c
Aadit Kamat <aadit.k12@gmail.com>
2022-04-23 03:14:08 +0000

    Fix style issues

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,8 +75,8 @@ module.exports = {
         {
           to: '/tools',
           label: 'Tools',
-          position: 'right'
-        }
+          position: 'right',
+        },
       ],
     },
     footer: {
