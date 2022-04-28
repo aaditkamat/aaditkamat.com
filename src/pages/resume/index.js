a255585efafc5d473525c921e5a28a171468a97f
Aadit Kamat <30969577+aaditkamat@users.noreply.github.com>
2022-04-28 13:21:43 +0000

    Update resume page

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -6,9 +6,15 @@ import styles from './styles.module.css';
 export default function Resume() {
     return (
         <Layout title="Résumé">
-            <div className={clsx("container", styles.container)}>
-                <h1 className={styles.header}> Résumé </h1>
-                <iframe src="../resume.pdf" width="100%" height="225%" />
+            <h1 className={styles.header}> Résumé </h1>
+            <div style={{aspectRatio: "16 / 9"}} className="container">
+                <iframe 
+                    src="../resume.pdf"
+                    width="100%"
+                    height="100%"
+                    frameborder="0"
+                >
+                </iframe>
             </div>
         </Layout>
     );
