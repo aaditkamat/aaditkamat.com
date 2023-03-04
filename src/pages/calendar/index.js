f55d18cf290b7790afe5aeacda2fec7ef1642a98
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2023-03-04 09:10:07 +0000

    Update calendar link

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -27,14 +27,7 @@ export default function Calendar() {
           <h1 className={styles.header}> Calendar </h1>
           <div className="row">
             <div className="col" style={{ paddingLeft: '20%' }}>
-              <iframe
-                src="https://calendar.google.com/calendar/embed?&wkst=1&src=aadit.k12%40gmail.com&ctz=Asia%2FSingapore"
-                className={styles.calendar}
-                width="800"
-                height="600"
-                frameborder="0"
-                scrolling="no"
-              ></iframe>
+            <iframe src="https://calendar.google.com/calendar/embed?src=aaditrahulkamat%40gmail.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
             </div>
           </div>
         </div>
